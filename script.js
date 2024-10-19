cacd2058f8b522506074961e5c24ff9ed36c7cb7
maryamusha <62988512+maryamusha@users.noreply.github.com>
2024-10-20 06:11:43 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Function to start balloon animation when page loads
-window.addEventListener('load', function() {
-    const balloons = document.querySelectorAll('.balloon');
-    balloons.forEach(balloon => {
-      balloon.style.animationPlayState = 'running';
-    });
+// You can use this script to trigger balloons fading after the load
+window.addEventListener('load', () => {
+    setTimeout(() => {
+      document.querySelector('.balloons-container').style.display = 'none';
+    }, 10000);  // 10 seconds
   });
